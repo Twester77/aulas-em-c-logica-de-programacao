efddc5a5add6958a85ce8d187493aff40ab08a67
Leonardo F <87826@unifev.edu.br>
2026-03-12 14:16:39 -0300

    feat: add program to determine youngest and oldest among five people

---
+++ lista-de exercicios-parte2/lista2-questao-10-semfor.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <locale.h>
+#include <windows.h>
+int main()
+{ 
+
+    long int  I1, I2, I3, I4, I5;
+    char nome1[31], nome2[31], nome3[31], nome4[31], nome5[31];
+
+printf (" Por favor informe o nome da primeira pessoa (até 30 caracteres):\n");
+scanf (" %30[^\n]", nome1);
+printf ("\nQual a data de nascimento dessa pessoa?(AAAAMMDD, escreva o numero por extenso sem espaço)\n");
+scanf ("%ld", &I1);
+
+printf ("\nPor favor informe o nome da segunda pessoa (até 30 caracteres):\n");
+scanf (" %30[^\n]", nome2);
+printf ("\nQual a data de nascimento dessa pessoa?(AAAAMMDD, escreva o numero por extenso sem espaço)\n");
+scanf ("%ld", &I2);
+
+printf ("\n Por favor informe o nome da terceira pessoa (até 30 caracteres):\n");
+scanf (" %30[^\n]", nome3);
+printf ("\nQual a data de nascimento dessa pessoa?(AAAAMMDD, escreva o numero por extenso sem espaço)\n");
+scanf ("%ld", &I3);
+
+printf ("\nPor favor informe o nome da quarta pessoa (até 30 caracteres):\n");
+scanf (" %30[^\n]", nome4);
+printf ("\nQual a data de nascimento dessa pessoa?(AAAAMMDD, escreva o numero por extenso sem espaço)\n");
+scanf ("%ld", &I4);
+
+printf ("\nPor favor informe o nome da quinta pessoa (até 30 caracteres):\n");
+scanf (" %30[^\n]", nome5);
+printf ("\nQual a data de nascimento dessa pessoa?(AAAAMMDD, escreva o numero por extenso sem espaço)\n");
+scanf ("%ld", &I5);
+
+
+if (I1 > I2 && I1 > I3 && I1 > I4 && I1 > I5) { printf("%s é a pessoa mais nova\n", nome1); }
+else if (I2 > I1 && I2 > I3 && I2 > I4 && I2 > I5) { printf("%s é a pessoa mais nova\n", nome2); }
+else if (I3 > I1 && I3 > I2 && I3 > I4 && I3 > I5) { printf("%s é a pessoa mais nova\n", nome3);}
+else if (I4 > I1 && I4 > I2 && I4 > I3 && I4 > I5) { printf("%s é a pessoa mais nova\n", nome4);}
+else { printf ("%s é a pessoa mais nova\n", nome5);} 
+
+if (I1 < I2 && I1 < I3 && I1 < I4 && I1 < I5) { printf("%s é a pessoa mais velha\n", nome2); }
+else if (I2 < I1 && I2 < I3 && I2 < I4 && I2 < I5) { printf("%s é a pessoa mais velha\n", nome2);}
+else if (I3 < I1 && I3 < I2 && I3 < I4 && I3 < I5) { printf("%s é a pessoa mais velha\n", nome3);} 
+else if (I4 < I1 && I4 < I2 && I4 < I3 && I4 < I5) { printf("%s é a  pessoa mais velha\n", nome4);}
+else { printf ("%s é a pessoa mais velha\n", nome5);}
+
+  
+return 0;
+
+ }
---
+++ lista-de exercicios-parte2/lista2-questao-9.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int  nome [30], I1, I2, I3, I4, I5;
+ 
+
+printf (" Por favor informe o nome da pessoa (até 30 caracteres):\n");
+scanf ("%s", &nome);
+printf ("\nQual a idade dessa pessoa?\n");
+scanf ("%d", I1 );
+
+
+for ( i = 1; i <= t2; i++) {
+    resul = t1 * i ;
+    printf(" A tabuada do %d e: %d * %d = %d\n", t1, t1, i, resul);
+ 
+}
+
+printf ("\n--------Fim do programa!---------\n");
+
+return 0;
+
+}
