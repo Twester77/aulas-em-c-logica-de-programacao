2a4cadedbbeb9f0b17baa1eeeaea8dd68c24c6b6
Leonardo F <87826@unifev.edu.br>
2026-03-22 22:57:51 -0300

    corrigindo ultimos detalhes

---
+++ lista-de exercicios-parte2/lista2-questao-13-soma-de-10-numeros.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+ int main()
+{ 
+ double soma = 0, N;
+ int i;
+
+printf("Vamos fazer uma soma com 10 numeros:\n");
+
+for (i = 1; i <= 10; i ++) {
+printf("Informe o numero:\n", i);
+scanf ("%lf",&N);
+soma = soma + N;
+}
+
+printf(" A soma total e: %.2lf\n", soma );
+printf("\n---------Fim do Programa!----------\n");
+
+
+    return 0;
+}
